1c0bb3c6137a5eeba1e5222891d658b22efb32c3
Marcloure <marcloure@outlook.com>
2020-02-28 04:04:47 -0300

    Added Completed Project

---
+++ 3D with 2D assets/Assets/Scripts/Billboard.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    void Update () {
+        // Forces the object to face the camera
+        transform.LookAt (Camera.main.transform.position, Vector3.up);
+    }
+}
---
+++ 3D with 2D assets/Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float degreesPerSec;
+    public Transform target;
+
+    private void Start () {
+        transform.LookAt (target);
+    }
+
+    void Update () {
+        transform.RotateAround (target.position, Vector3.up, degreesPerSec * Time.deltaTime);
+    }
+}
